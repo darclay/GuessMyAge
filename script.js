13a523cc631723a6bb6ddf8f5e08ed5a5acf7010
David Gurule <davidgurulejr@gmail.com>
2022-01-19 16:56:21 -0700

    added the logic

--- script.js
+++ script.js
@@ -1,16 +1,43 @@
 "use strict"
-
-let randomNum = Math.ceil(Math.random()*100);
-console.log(randomNum);
+let randomNum = Math.ceil(Math.random()*10);
+let score = 20;
+let highScore = 20;
 
 const handleSubmit = () => {
   let guessedNum = Number(document.querySelector('#numInput').value);
-  console.log(guessedNum);
+ 
   if(guessedNum === randomNum){
-      console.log('congrats you guessed right');
-  }else {
-    console.log(`You guessed WRONG`)
+      score += 1;
+      document.querySelector('#playerScore').textContent = score;
+      document.querySelector('#highLowIndicator').textContent = 'YOU ARE CORRECT!'
+      document.querySelector('#guessedNumber').textContent = randomNum;
+      randomNum = Math.ceil(Math.random()*10);
+      document.querySelector('body').style.backgroundColor = '#A9E2A5';
+  }else if(guessedNum < randomNum){
+    score -= 1;
+    document.querySelector('#playerScore').textContent = score;
+    document.querySelector('#highLowIndicator').textContent = 'You guessed low!'
+    document.querySelector('#guessedNumber').textContent = '?';
+    document.querySelector('body').style.backgroundColor = 'rgb(223, 153, 153)';
+  }else if(guessedNum > randomNum){
+    score -= 1;
+    document.querySelector('#playerScore').textContent = score;
+    document.querySelector('#highLowIndicator').textContent = 'You guessed high!'
+    document.querySelector('#guessedNumber').textContent = '?';
+    document.querySelector('body').style.backgroundColor = 'rgb(223, 153, 153)';
+  }
+  
+  if (score < 1){
+    document.querySelector('.active').className = 'notActive';
+    document.querySelector('.notActive').className = 'active';
+    document.querySelector('body').style.backgroundColor = '#7E808D';
+  }
+  if(score > highScore){
+    highScore = score;
+    document.querySelector('#highScore').textContent = highScore;
   }
 }
 
-document.querySelector('.submitButton').addEventListener('click', handleSubmit);
+
+document.querySelector('#submitButton').addEventListener('click', handleSubmit);
+document.querySelector('#youLose').addEventListener('click',()=>window.location.reload());
