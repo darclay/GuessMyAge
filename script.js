713cba8a5c88f723134189e4f444197ac6e09f0e
David Gurule <davidgurulejr@gmail.com>
2022-01-22 13:15:25 -0700

    added a ternary to the < and > values in order to not repeat code.

--- script.js
+++ script.js
@@ -13,20 +13,13 @@ const handleSubmit = () => {
       document.querySelector('#guessedNumber').textContent = randomNum;
       randomNum = Math.ceil(Math.random()*10);
       document.querySelector('body').style.backgroundColor = '#A9E2A5';
-  }else if(guessedNum < randomNum){
+  }else if(guessedNum !== randomNum){
     score -= 1;
     document.querySelector('#playerScore').textContent = score;
-    document.querySelector('#highLowIndicator').textContent = 'You guessed low!'
-    document.querySelector('#guessedNumber').textContent = '?';
-    document.querySelector('body').style.backgroundColor = 'rgb(223, 153, 153)';
-  }else if(guessedNum > randomNum){
-    score -= 1;
-    document.querySelector('#playerScore').textContent = score;
-    document.querySelector('#highLowIndicator').textContent = 'You guessed high!'
+    document.querySelector('#highLowIndicator').textContent = guessedNum < randomNum ? 'You guessed low!' : 'You guessed high!';
     document.querySelector('#guessedNumber').textContent = '?';
     document.querySelector('body').style.backgroundColor = 'rgb(223, 153, 153)';
   }
-  
   if (score < 1){
     document.querySelector('.active').className = 'notActive';
     document.querySelector('.notActive').className = 'active';
